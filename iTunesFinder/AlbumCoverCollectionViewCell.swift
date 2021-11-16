679fcefd9238830881088e06cc022a6ade735065
ILYA <79190746+cujah@users.noreply.github.com>
2021-11-16 11:11:23 +0300

    First Step

---
+++ iTunesFinder/AlbumCoverCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AlbumCoverCollectionViewCell.swift
+//  iTunesFinder
+//
+//  Created by Илья on 16.11.2021.
+//
+
+import UIKit
+
+class AlbumCoverCollectionViewCell: UICollectionViewCell {
+    
+}
