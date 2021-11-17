506c83beddb51c815d4290abaa23a31468e06fb3
ILYA <79190746+cujah@users.noreply.github.com>
2021-11-17 12:00:56 +0300

    add tab bar and history table view

---
+++ iTunesFinder/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableViewCell.swift
+//  iTunesFinder
+//
+//  Created by Илья on 17.11.2021.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
